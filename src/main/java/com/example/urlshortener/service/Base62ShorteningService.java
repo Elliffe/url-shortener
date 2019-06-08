c5beafcd9011116c2d25cd7fcbf44dbb78eee4fa
Robert <robert@vbot.tv>
2019-06-08 19:25:33 +0100

    Add decodeId function to get actual id from Base62 encoded id. Refactor to accommodate long and to improve readability.

--- src/main/java/com/example/urlshortener/service/Base62ShorteningService.java
+++ src/main/java/com/example/urlshortener/service/Base62ShorteningService.java
@@ -1,37 +1,32 @@
 package com.example.urlshortener.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.example.urlshortener.model.Url;
 import org.springframework.stereotype.Repository;
-
-import java.net.URI;
 
 @Repository("Base62")
 public class Base62ShorteningService implements ShorteningService {
 
-    private final CounterService counterService;
-
-    @Autowired
-    public Base62ShorteningService(@Qualifier("local") CounterService counterService) {
-        this.counterService = counterService;
-    }
+    private static final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     @Override
-    public String encodeUrl(URI longUrl) {
-        String base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-        int id = this.counterService.getCounter();
-
+    public String encodeUrl(Url url) {
+        long id = url.getId();
         StringBuilder sb = new StringBuilder();
         while (id != 0) {
-            sb.append(base62.charAt(id % 62));
+            long rem = id % 62;
+            sb.append(BASE62.charAt((int)rem));
             id /= 62;
         }
         return sb.reverse().toString();
+
     }
 
     @Override
-    public String decodeUrl(URI longUrl) {
-        return null;
+    public long decodeId(String encodedId) {
+        long decodedId = 0;
+        for(int i = 0; i < encodedId.length(); i++) {
+            decodedId = decodedId * 62 + BASE62.indexOf("" + encodedId.charAt(i));
+        }
+        return decodedId;
     }
 }
