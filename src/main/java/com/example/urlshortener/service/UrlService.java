05026335a1a828ad171699d946a613e4dd48a272
Robert <robert@vbot.tv>
2019-06-08 13:10:31 +0100

    Add a Url service to access the Url Dao

---
+++ src/main/java/com/example/urlshortener/service/UrlService.java
@@ -0,0 +1,22 @@
+package com.example.urlshortener.service;
+
+import com.example.urlshortener.dao.UrlDao;
+import com.example.urlshortener.model.Url;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlService {
+
+    private final UrlDao urlDao;
+
+    @Autowired
+    public UrlService(@Qualifier("ArrayListDao") UrlDao urlDao) {
+        this.urlDao = urlDao;
+    }
+
+    public Url insertUrl(Url url) {
+        return urlDao.insertUrl(url);
+    }
+}
