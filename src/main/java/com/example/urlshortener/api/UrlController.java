3a2837c8f8e78d8f24d197a2bfe6bdf176abbcfa
Robert <robert@vbot.tv>
2019-06-10 22:03:23 +0100

    Move the counter request to the controller. Handle bad requests to the API. Add tests for the controller.

--- src/main/java/com/example/urlshortener/api/UrlController.java
+++ src/main/java/com/example/urlshortener/api/UrlController.java
@@ -1,16 +1,19 @@
 package com.example.urlshortener.api;
 
 import com.example.urlshortener.model.Url;
+import com.example.urlshortener.service.CounterService;
 import com.example.urlshortener.service.ShorteningService;
 import com.example.urlshortener.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("")
 @RestController
@@ -18,30 +21,38 @@ public class UrlController {
 
     private final UrlService urlService;
     private final ShorteningService shorteningService;
+    private final CounterService counterService;
     @Value("${hostname}")
     private String hostname;
 
     @Autowired
-    public UrlController(UrlService urlService, @Qualifier("Base62") ShorteningService shorteningService) {
+    public UrlController(UrlService urlService,
+                         @Qualifier("Base62") ShorteningService shorteningService,
+                         CounterService counterService) {
         this.urlService = urlService;
         this.shorteningService = shorteningService;
+        this.counterService = counterService;
     }
 
     @PostMapping
-    public Map<String, String> insertUrl(@RequestBody Url request) {
-        Url url = urlService.insertUrl(request);
+    @ResponseBody
+    public ResponseEntity<String> insertUrl(@RequestBody Url request, HttpServletResponse response) {
+        Long id = counterService.getCounter();
+        if(id == null) { return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE); }
+
+        Url url = new Url(id, request.getUrl());
+        urlService.insertUrl(url);
         String shortUrl = hostname.concat("/").concat(shorteningService.encodeUrl(url));
 
-        HashMap<String, String> map = new HashMap<>();
-        map.put("long", url.getUrl().toString());
-        map.put("short", shortUrl);
-        return map;
+        return new ResponseEntity<>(shortUrl, HttpStatus.OK);
     }
 
     @GetMapping(path = "{id}")
     public ModelAndView method(@PathVariable("id") String id) {
         long urlId = shorteningService.decodeId(id);
         Url fullUrl = urlService.getUrl(urlId);
+        if(fullUrl == null) { throw new ResponseStatusException(HttpStatus.NOT_FOUND, "URL Not Found"); }
+
         return new ModelAndView("redirect:" + fullUrl.getUrl());
     }
 }
---
+++ src/test/java/com/example/urlshortener/api/UrlControllerTests.java
@@ -0,0 +1,170 @@
+package com.example.urlshortener.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UrlControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnShortUrls() throws Exception {
+        Object randomObj = new Object() {
+            public final String url = "http://www.example.com";
+        };
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(randomObj);
+
+
+        MvcResult result = this.mockMvc.perform(
+            post("/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        String body = result.getResponse().getContentAsString();
+        Assert.assertEquals(body, "http://localhost:8080/a");
+
+        result = this.mockMvc.perform(
+                post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        body = result.getResponse().getContentAsString();
+        Assert.assertEquals(body, "http://localhost:8080/b");
+    }
+
+    @Test
+    public void shouldReturnLongUrl() throws Exception {
+        String longUrl = "http://www.example.com";
+        Object obj = new Object() {
+            public final String url = longUrl;
+        };
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(obj);
+
+
+        MvcResult result = this.mockMvc.perform(
+                post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String body = result.getResponse().getContentAsString();
+
+        String idStr = body.substring(body.lastIndexOf('/') + 1);
+
+        this.mockMvc.perform(
+                get("/"+idStr)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl(longUrl))
+                .andReturn();
+
+
+        // Submit a second URL and test the returned short URL
+        String longUrl2 = "http://www.google.com";
+        Object obj2 = new Object() {
+            public final String url = longUrl2;
+        };
+
+        objectMapper = new ObjectMapper();
+        json = objectMapper.writeValueAsString(obj2);
+
+
+        result = this.mockMvc.perform(
+                post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        body = result.getResponse().getContentAsString();
+
+        idStr = body.substring(body.lastIndexOf('/') + 1);
+
+        this.mockMvc.perform(
+                get("/"+idStr)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl(longUrl2))
+                .andReturn();
+    }
+
+    @Test
+    public void shouldRejectInjectionUrls() throws Exception {
+        // Test for character "{"
+        Object obj = new Object() {
+            public final String url = "{";
+        };
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(obj);
+
+
+        this.mockMvc.perform(
+                post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andReturn();
+
+        // Test for character "}"
+        obj = new Object() {
+            public final String url = "}";
+        };
+
+        objectMapper = new ObjectMapper();
+        json = objectMapper.writeValueAsString(obj);
+
+
+        this.mockMvc.perform(
+                post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andReturn();
+
+        // Test for character "\"
+        obj = new Object() {
+            public final String url = "\\";
+        };
+
+        objectMapper = new ObjectMapper();
+        json = objectMapper.writeValueAsString(obj);
+
+
+        this.mockMvc.perform(
+                post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andReturn();
+    }
+}
